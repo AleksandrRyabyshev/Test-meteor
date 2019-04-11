96fe1d70007d0dffa3a0a553f7f8d7c70ae932dc
Ryabyshev Aleksandr <ssori3838@gmail.com>
2019-04-11 21:56:23 +0300

    Add Test

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
+// import { Meteor } from 'meteor/meteor';
 import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 import Task from './Task.js';
@@ -28,26 +29,39 @@ class App extends Component {
         // Find the text field via the React ref
         const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-        Tasks.insert({
-            text,
-            createdAt: new Date(), // current time
-            owner: Meteor.userId(),           // _id of logged in user
-            username: Meteor.user().username,  // username of logged in user
-        });
+        Meteor.call('tasks.insert', text);
+        // Tasks.insert({
+        //     text,
+        //     createdAt: new Date(), // current time
+        //     owner: Meteor.userId(),           // _id of logged in user
+        //     username: Meteor.user().username,  // username of logged in user
+        // });
 
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
     renderTasks() {
-        let filteredTask = this.props.tasks;
+        let filteredTasks = this.props.tasks;
         if (this.state.hideCompleted) {
-           filteredTask = filteredTask.filter(task => !task.checked);
+            filteredTasks = filteredTasks.filter(task => !task.checked);
 
         }
-        return filteredTask.map((task) => (
-            <Task key={task._id} task={task}/>
-        ));
+        return filteredTasks.map((task) => {
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButton = task.owner === currentUserId;
+
+            return (
+                <Task
+                    key={task._id}
+                    task={task}
+                    showPrivateButton={showPrivateButton}
+                />
+            );
+        });
+        // return filteredTask.map((task) => (
+        //     <Task key={task._id} task={task}/>
+        // ));
     }
 
     render() {
@@ -93,6 +107,7 @@ class App extends Component {
 }
 
 export default withTracker(() => {
+    Meteor.subscribe('tasks');
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
         incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
